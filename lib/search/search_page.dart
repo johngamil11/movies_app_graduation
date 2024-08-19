a3e2e71db3ed1af021bea7bb0d29ad49683934fb
Kimo Store <johngamil52@gmail.com>
2024-08-19 22:56:56 +0200

    finished  splash screen and tabs screen.

--- lib/search/search_page.dart
+++ lib/search/search_page.dart
@@ -4,7 +4,7 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.cyan,
+      color: Colors.pink,
     );
   }
 }
