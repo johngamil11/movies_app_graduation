ee89db40b1558a1e18e102841ef66b8559841bb9
ALAA <mohamd.elbanna.25@gmail.com>
2024-08-20 23:41:58 +0300

    Finished New releases widget in Home Tab added comment for testing github

---
+++ lib/api/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'api.themoviedb.org';
+
+  static const String upcomingApi = '/3/movie/upcoming';
+
+  /// testing api on github
+}
---
+++ lib/home/new_releases/newReleasesWidget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app_team/home/new_releases/poster_widget.dart';
+
+import '../../colors.dart';
+
+class NewReleasesWidget extends StatelessWidget {
+  var resultList;
+
+  String path = "https://image.tmdb.org/t/p/w500";
+
+  NewReleasesWidget({required this.resultList});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Container(
+      height: height * 0.25,
+      color: AppColor.widgetBackground,
+      padding: EdgeInsets.only(top: 10, bottom: 10, left: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "New Releases",
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.01),
+                  child: PosterWidget(
+                      poster: path + resultList[index].posterPath ?? ""),
+                );
+              },
+              itemCount: resultList.length,
+              scrollDirection: Axis.horizontal,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
